6c04216dfddb679f320bbaca2ce10bcbe17e763e
Kevin Bowers <kevin.p.bowers@gmail.com>
2019-12-02 02:11:16 -0600

    Fixed update tour_stop query

--- update_table.sql
+++ update_table.sql
@@ -3,9 +3,7 @@
 
 UPDATE tour_stop 
     SET tour_stop_location = 'Columbia, MO'
-    WHERE tour_stop_location = 'Nashville, TN' IN (SELECT tour_stop_location
-                                                   FROM tour_stop
-                                                   WHERE tour_name = 'Cuz I Love You Too');
+    WHERE tour_stop_location = 'Nashville, TN' AND tour_name = "Cuz I Love You Too";
     
 UPDATE artist_socials
     SET instagram = 'lizzo'
